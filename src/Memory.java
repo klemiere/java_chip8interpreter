cfd5fdc4e52c5c94a25bcad67544248f2655d66c
klemiere <lemierekevin1@gmail.com>
2025-05-10 16:32:05 +0200

    Create Memory class
    Add the array that represents the memory
    Add read and write methods
    Add loadRom method

---
+++ src/Memory.java
@@ -0,0 +1,38 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Memory {
+    /*chip8 can access up to 4kb of ram, from locations 0x000 to 0xfff, original interpreter located at 0x000 to 0x1ff,
+    do not use.
+    roms typically start at 0x200, 0x600 is also possible
+     */
+    private final int[] ram = new int[4096];
+    String romPath;
+
+    Memory(String romPath){
+        this.romPath = romPath;
+        loadRom(romPath);
+    }
+
+    private void loadRom(String romPath){
+        try {
+            InputStream inputStream = new FileInputStream(romPath);
+            int byteCount = 512; //assume the rom starts at 0x200
+            while(inputStream.read() != -1){
+                write(byteCount, inputStream.read());
+                byteCount++;
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public int read(int address){
+        return ram[address];
+    }
+
+    public void write(int address, int value){
+        ram[address] = value;
+    }
+}
