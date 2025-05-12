386194dcfb53eb68802beda41093f0bf00f04c24
klemiere <lemierekevin1@gmail.com>
2025-05-12 20:12:06 +0200

    Add SCALE variable and constructor
    omg a window

--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,15 @@
-public class Display {
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JPanel {
+
     public static final int HEIGHT = 64;
     public static final int WIDTH = 32;
+    public static final int SCALE = 10; //Scale multiplier for the pixels
+
+    Display(){
+        this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+    }
 }
