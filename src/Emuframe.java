25480533a0dfc4f4f365ff4efb130db308cfc101
klemiere <lemierekevin1@gmail.com>
2025-06-14 14:26:19 +0200

    Feed display created outside of constructor instead of creating it in the constructor

--- src/Emuframe.java
+++ src/Emuframe.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 
 public class Emuframe extends JFrame {
-    Emuframe(){
-        this.add(new Display());
+    Emuframe(Display display){
+        this.add(display);
         this.setTitle("Chip8");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
