ea1c3499686bc2e9db1f5b123d7bb05190d35341
klemiere <lemierekevin1@gmail.com>
2025-06-14 14:27:11 +0200

    Implement flipPixel method and override paintComponent
    Also switched WIDTH and HEIGHT around for the time being

--- src/Display.java
+++ src/Display.java
@@ -7,7 +7,7 @@ public class Display extends JPanel {
     public static final int WIDTH = 32;
     public static final int SCALE = 10; //Scale multiplier for the pixels
 
-    private boolean display[][] = new boolean[HEIGHT][WIDTH];
+    private boolean display[][] = new boolean[WIDTH][HEIGHT];
 
     Display(){
         this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
@@ -16,6 +16,29 @@ public class Display extends JPanel {
     }
 
     public void clear(){
-        display = new boolean[HEIGHT][WIDTH];
+        display = new boolean[WIDTH][HEIGHT];
+    }
+
+    public boolean flipPixel(int x, int y){
+        x = x % WIDTH;
+        y = y % HEIGHT;
+        boolean current = display[x][y];
+        display[x][y] = !current;
+        return current;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // clear background
+
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                if (display[x][y]) {
+                    g.setColor(Color.WHITE);
+                    g.fillRect(x * SCALE, y * SCALE, SCALE, SCALE);
+                }
+                // else: skip or paint background (already cleared)
+            }
+        }
     }
 }
